ca533df668e879f4785299daa51665fbb2fbc543
vistritpandey <vistritpandey@gmail.com>
2020-11-26 12:06:20 +0530

    k

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet  } from 'react-native'
+import { View, Text, StyleSheet, ActivityIndicator  } from 'react-native'
+import firebase from 'firebase'
 
 export default class LoadingScreen extends React.Component {
+    
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            this.props.navigation.navigate(user ? "App" : "Auth")
+        })
+    }
+    
+    
+    
     render() {
         return (
             <View style={styles.container}>
                 <Text>Loading......</Text>
+                <ActivityIndicator size="large"></ActivityIndicator>
             </View>
         )
     }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet  } from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 
 export default class LoginScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Loading......</Text>
+                <Text style={styles.greeting}>{`Hello there\nWelcome`}</Text>
+                <View style={styles.errorMessage}>
+                    <Text>Error</Text>
+                </View>
+
+                <View style={styles.form}>
+                    <View>
+                        <Text style={styles.inputTitle}>Email Address</Text>
+                        <TextInput style={styles.input} autoCapatalize="none"></TextInput>
+                    </View>
+
+                    <View style={{ marginTop: 32 }}>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <TextInput style={styles.input} autoCapatalize="none"></TextInput>
+                    </View>
+
+                </View>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text>
+                        Sign In
+                    </Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -14,8 +37,34 @@ export default class LoginScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent:"center",
-        alignItems: "center"
+        flex: 1
+    },
+    greeting : {
+        marginTop: 32,
+        fontSize: 18,
+        fontWeight: "400",
+        textAlign: "center"
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 30
+    },
+    form: {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+        color: "#8A8F9E",
+        fontSize: 10,
+        textTransform: "uppercase"
+    },
+    input: {
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D"
     }
 })
