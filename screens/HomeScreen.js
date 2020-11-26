d9e899e47e63a150369aaf32c107659d81dc701e
vistritpandey <vistritpandey@gmail.com>
2020-11-26 12:50:49 +0530

    k

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet  } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity  } from 'react-native'
 import firebase from 'firebase'
 
 export default class HomeScreen extends React.Component {
 
+    state = {
+        email: "",
+        displayName: ""
+    };
+
+    componentDidMount(){
+        const { email, displayName } = firebase.auth().currentUser;
+        this.setState({email, displayName});
+    }
+
+    signOutUser = () => {
+        firebase.auth().signOut();
+    };
     
     render() {
         return (
             <View style={styles.container}>
-                <Text>Loading......</Text>
+                <Text>Hey, {this.state.email}</Text>
+
+                <TouchableOpacity style={{ marginTop: 32 }} onPress = {this.signOutUser}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
             </View>
         )
     }
